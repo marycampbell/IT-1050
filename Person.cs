b46cd344815718c3d8fc0087d9b7e14bce12ae0c
Mary Campbell <campbellm.2015@gmail.com>
2018-04-03 20:36:15 -0400

    asdf
    asdf

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_two
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(LastName + "," + FirstName + " (" + Age + ") ");
+        }
+
+    }
+}
