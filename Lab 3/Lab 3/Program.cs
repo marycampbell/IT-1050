549a919d2bc8a7b761b0d08e31ee51b3755a3ddc
Mary Campbell <campbellm.2015@gmail.com>
2018-04-09 21:00:37 -0400

    ...
    ...

---
+++ Lab 3/Lab 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var John = new Instructor("John" , "English");
+            var Mike = new Instructor("Mike" , "Math");
+
+            var Jane = new Student("Jane" , John);
+            var Joe = new Student("Joe" , John);
+            var Melissa = new Student("Melissa" , Mike);
+            var Matt = new Student("Matt" , Mike);
+
+            John.SetStudentGrade(Jane, 95);
+            John.SetStudentGrade(Joe, 85);
+
+            Mike.SetStudentGrade(Melissa, 90);
+            Mike.SetStudentGrade(Matt, 92);
+
+            Jane.Print();
+            Joe.Print();
+            Melissa.Print();
+            Matt.Print();
+
+            System.Console.ReadLine();
+
+               
+            
+        }
+    }
+}
---
+++ Lab 3/Lab 3/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public Student(string name, Instructor teacher)
+        {
+            this.Name = name;
+            this.Teacher = teacher;
+            this.Grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            this.Grade = grade;
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine(this.Name + ":");
+            System.Console.WriteLine("I am a student named " + this.Name + ".");
+            System.Console.WriteLine("My grade was " + this.Grade + ".");
+            this.Teacher.Print();
+        }
+
+
+
+    }
+}
